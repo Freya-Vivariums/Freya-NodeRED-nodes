4fc0f64688b1341943fe33ed6b4a7de1191ac279
Tom Santens <tom.santens@gmail.com>
2025-07-03 09:54:29 +0200

    fontawesome icons + Freya Vivariums category

--- nodes/circadian-engine/circadian-engine.js
+++ nodes/circadian-engine/circadian-engine.js
@@ -2,7 +2,7 @@ module.exports = function(RED) {
   function CircadianEngineNode(config) {
     RED.nodes.createNode(this, config);
     const node = this;
-
+    node.status({ fill:"green", shape:"dot", text:"running" });
     node.on('input', async function(msg) {
       node.send(msg);
     });
