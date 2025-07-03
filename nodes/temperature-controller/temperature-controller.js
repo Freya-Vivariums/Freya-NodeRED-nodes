eb1a0f45aabf0e176e146b6106b33252e74bc20a
Tom Santens <tom.santens@gmail.com>
2025-07-03 09:38:56 +0200

    TempCtrl: fontawesome icon

--- nodes/temperature-controller/temperature-controller.js
+++ nodes/temperature-controller/temperature-controller.js
@@ -2,7 +2,7 @@ module.exports = function(RED) {
   function TemperatureControllerNode(config) {
     RED.nodes.createNode(this, config);
     const node = this;
-
+    node.status({ fill:"green", shape:"dot", text:"running" });
     node.on('input', async function(msg) {
       node.send(msg);
     });
